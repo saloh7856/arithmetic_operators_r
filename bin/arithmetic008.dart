855611fa75fa345c6782dd833bf1a62ba746905a
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-07-14 11:32:07 +0500

    8

--- bin/arithmetic008.dart
+++ bin/arithmetic008.dart
@@ -2,9 +2,9 @@
 // the task condition is in the Readme file
 import 'dart:math';
 num func(num a,num b){
-    return pow(a,2)*pow(b,2);
+    return  (pow(a,2))*pow(b,2);
 }
 void main(){
-    
+
     print(func(2,5));
 }
