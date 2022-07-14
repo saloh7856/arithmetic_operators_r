1fa84c46f396565a2ef26e5dda03253832045bc2
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-07-14 13:15:29 +0500

    35

--- bin/arithmetic035.dart
+++ bin/arithmetic035.dart
@@ -2,8 +2,8 @@
 // the task condition is in the Readme file
 import 'dart:math';
 num func(num a,num b){
-    
-    return (a*b*b*a)*((a+b)/(a*b));
+
+    return ((a*b*b*a)*((a+b)/(a*b)))*(3/8);
 }
 void main(){
 
