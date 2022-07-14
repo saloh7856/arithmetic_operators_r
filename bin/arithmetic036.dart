a2fef16f1177ef4128281923c86290f74fb32335
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-07-14 13:17:59 +0500

    36

--- bin/arithmetic036.dart
+++ bin/arithmetic036.dart
@@ -1,4 +1,11 @@
 // Create function func with arguments
 // the task condition is in the Readme file
 
-void main() {}
+import 'dart:math';
+num func(num a,num b){
+    return 2*pow(a,b);
+}
+void main(){
+
+    print(func(2,4));
+}
